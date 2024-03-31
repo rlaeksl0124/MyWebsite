97f3e73d9c21e9d3e366577f54187da4fd49ad7a
rlaeksl0124 <75132079+rlaeksl0124@users.noreply.github.com>
2024-03-31 22:32:12 +0900

    User Login Logout & Boardlist upload

---
+++ src/main/java/com/fastcampus/ch3/BoardController.java
@@ -0,0 +1,28 @@
+package com.fastcampus.ch3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+    @GetMapping("/list")
+    public String list(HttpServletRequest request){
+        String toURL = request.getRequestURL().toString();
+
+        if(!loginCheck(request)){
+            return "redirect:/user/login?toURL="+toURL;
+        }
+
+        return "boardList";
+    }
+
+    private boolean loginCheck(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        return session.getAttribute("userId")!=null;
+    }
+}
